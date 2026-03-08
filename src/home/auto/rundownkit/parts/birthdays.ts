bc8c819b2142a3b740f9db044d97b278c2586d89
KTibow <KTibow@users.noreply.github.com>
2026-03-08 15:05:10 -0700

    more birthdays

--- src/home/auto/rundownkit/parts/birthdays.ts
+++ src/home/auto/rundownkit/parts/birthdays.ts
@@ -1,6 +1,9 @@
 import birthdaysBySchool from 'virtual:birthdays';
 import type { RundownPart } from '../+main.svelte';
 
+const formatBirthday = (date: Date) =>
+  `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
+
 export const birthdaysRundownPart = {
   label: 'Updating birthdays',
   matchesLine: (line) => line.startsWith('- Happy birthday '),
@@ -15,10 +18,19 @@ export const birthdaysRundownPart = {
       throw new Error('Unknown school');
     }
 
-    const todayFormatted = `${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getDate().toString().padStart(2, '0')}`;
+    const birthdaysToShow = [today];
+    if (today.getDay() == 5) {
+      birthdaysToShow.push(
+        new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1),
+        new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2),
+      );
+    }
 
-    return teachers
-      .filter((teacher) => schoolBirthdays[teacher] == todayFormatted)
+    const todayish = new Set(birthdaysToShow.map(formatBirthday));
+    const teachersToCheck = Array.from(new Set([...teachers, 'Kurt Criscione']));
+
+    return teachersToCheck
+      .filter((teacher) => todayish.has(schoolBirthdays[teacher]))
       .map((teacher) => `Happy birthday ${teacher}`);
   },
 } satisfies RundownPart;
